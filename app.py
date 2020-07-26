cb9b1b903ddaffa51c56b7c30e285fdcc886a12e
avigupta2612 <avigupta2612@gmail.com>
2020-07-27 01:00:06 +0530

    null

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import os
 examples_path = os.listdir('datasets/')
 plots_list = {
     'Histogram': plot_histogram, 
+    'Bar Chart': plot_barplot,
     'Scatter Plot': plot_scatterplot,
     'Box Plot': plot_boxplot       
 }
@@ -40,11 +41,25 @@ if option:
     if target_col is not None:
         if st.sidebar.checkbox('Target Histogram', value=1):
             plot_histogram(df, target_col, target_col)
-    
+    if st.sidebar.checkbox('Check for Missing Values'):
+        null = df.isnull().values.any()
+        if null:
+            missing_values = df.isna().sum()
+            missing_values = missing_values[missing_values>0]
+            missing_values.sort_values(inplace=True)
+            missing_values = missing_values.to_frame()
+            missing_values.columns = ['Count']
+            missing_values.index.names = ['Name']
+            missing_values['Name'] = missing_values.index
+            plot_barplot(missing_values, xcol='Name', ycol= 'Count', color='Name')
+            
+        else:
+            st.info('No missing values')
     plot = st.sidebar.selectbox(
         'Select Plot',
         (None, (*plots_list.keys()))
     )
+    
     if plot:
         xcol = st.sidebar.selectbox(
             'Select x axis column',
--- plots.py
+++ plots.py
@@ -6,16 +6,24 @@ import numpy as np
 def plot_histogram(df, xcol=None,y_col=None, color = None):
     if color is None:
         color = xcol
-    fig = px.histogram(df, x=xcol, title= 'Target column Histogram', 
-                                color = color, color_discrete_sequence= px.colors.sequential.Viridis)
+    fig = px.histogram(df, x=xcol, title= 'Histogram', 
+                                color = df[color].values, color_discrete_sequence= px.colors.sequential.Viridis)
     st.plotly_chart(fig)
 
 def plot_scatterplot(df, xcol= None, ycol = None, color = None):
-    fig = px.scatter(df, xcol, ycol, color, color_discrete_sequence= px.colors.sequential.Viridis)
+    fig = px.scatter(df, xcol, ycol, color, 
+                                title= 'Scatter Plot', color_discrete_sequence= px.colors.sequential.Viridis)
     st.plotly_chart(fig)
 
 def plot_boxplot(df, xcol=None, ycol = None, color = None):
     if color is None:
         color = xcol
-    fig = px.box(df, xcol, ycol, color, color_discrete_sequence= px.colors.sequential.Viridis)
+    fig = px.box(df, xcol, ycol, color, 
+                                title = 'Box Plot', color_discrete_sequence= px.colors.sequential.Viridis)
+    st.plotly_chart(fig)
+
+def plot_barplot(df, xcol=None, ycol= None, color= None):
+    fig = px.bar(df, xcol, ycol, color ,
+                                title= 'Bar Chart', color_discrete_sequence= px.colors.sequential.Plasma
+                                )
     st.plotly_chart(fig)
